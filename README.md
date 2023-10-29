201749bbac668e4233558ee32098826fccb9c173
E Sequeira <5458743+geseq@users.noreply.github.com>
2023-10-29 22:01:18 +0000

    fix readme

--- README.md
+++ README.md
@@ -11,18 +11,14 @@ are tracked separately and are properly deallocated when released back to the po
 these extras will introduce some allocation penalty but will enable the pool to continue functioning.
 
 The release method includes a runtime check to ensure that objects are not released into a full pool.
-If an attempt is made to release an object into a full pool, a std::runtime_error will be thrown.
 
 Note: This implementation is not thread-safe. If thread-safe behavior is required,
 additional synchronization mechanisms must be added.
-
-Exception Handling:
-- The release() method may throw a std::runtime_error if it detects an attempt to release an object into a full pool.
 
 Usage:
 ```cpp
 AdaptivePobjectPool<MyObject, 100> pool;
 MyObject* obj = pool.acquire();
 // Use obj...
-pool.release(obj);  // May throw std::runtime_error if pool is full.
+pool.release(obj);
 ```
--- include/pool.hpp
+++ include/pool.hpp
@@ -19,19 +19,15 @@ namespace pool {
  * these extras will introduce some allocation penalty but will enable the pool to continue functioning.
  *
  * The release method includes a runtime check to ensure that objects are not released into a full pool.
- * If an attempt is made to release an object into a full pool, a std::runtime_error will be thrown.
  *
  * Note: This implementation is not thread-safe. If thread-safe behavior is required,
  * additional synchronization mechanisms must be added.
- *
- * Exception Handling:
- * - The release() method may throw a std::runtime_error if it detects an attempt to release an object into a full pool.
  *
  * Usage:
  * AdaptivePobjectPool<MyObject, 100> pool;
  * MyObject* obj = pool.acquire();
  * // Use obj...
- * pool.release(obj);  // May throw std::runtime_error if pool is full.
+ * pool.release(obj);
  *
  * Template Parameters:
  * T - The type of object the pool will manage.
